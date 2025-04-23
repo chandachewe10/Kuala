2377c7916d525dc4862a4b328d1920421421052b
chandachewe10 <chewec03@gmail.com>
2025-04-23 09:26:07 +0200

    Create observer to Run AI Validation after Claim has been created

--- app/Models/Claim.php
+++ app/Models/Claim.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class Claim extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'full_name',
+        'email',
+        'phone',
+        'policy_number',
+        'type',
+        'incident_date',
+        'incident_location',
+        'description',
+        'estimate_loss',
+        'police_report_number',
+        'witnesses',
+        'ai_feedback',
+        'userId',
+        'attachment',
+       
+    ];
 }
---
+++ app/Observers/ClaimObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Claim;
+
+class ClaimObserver
+{
+    /**
+     * Handle the Claim "created" event.
+     */
+    public function created(Claim $claim): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Claim "updated" event.
+     */
+    public function updated(Claim $claim): void
+    {
+        $claim->runAiValidation(); 
+    }
+
+    /**
+     * Handle the Claim "deleted" event.
+     */
+    public function deleted(Claim $claim): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Claim "restored" event.
+     */
+    public function restored(Claim $claim): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Claim "force deleted" event.
+     */
+    public function forceDeleted(Claim $claim): void
+    {
+        //
+    }
+}
