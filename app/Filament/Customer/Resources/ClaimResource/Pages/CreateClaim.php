f941dee2f5dfa141f9f01df312c3071a036cd556
chandachewe10 <chewec03@gmail.com>
2025-04-24 05:10:26 +0200

    Add Email Notification when the client submits a Claim

--- app/Filament/Customer/Resources/ClaimResource/Pages/CreateClaim.php
+++ app/Filament/Customer/Resources/ClaimResource/Pages/CreateClaim.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Customer\Resources\ClaimResource\Pages;
 
 use App\Filament\Customer\Resources\ClaimResource;
+use App\Notifications\ClaimStatusNotication;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Auth;
@@ -16,6 +17,11 @@ class CreateClaim extends CreateRecord
         $data['userId'] = Auth::user()->id;
        
         return $data;
+
+        $user = auth()->user();
+        $message = 'Hello ' . $user->name . ', your claim has been submitted successfully and is now pending review. You will be updated via email once it is reviewed.';
+        $user->notify(new ClaimStatusNotification($message));
+        
     }
 
 
